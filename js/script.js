8c1a3498f0c7af0f0838ef1483c3ebdcb2be7c51
renadnajj <renadnajj24@gmail.com>
2022-09-20 17:44:38 +0300

    html,css,js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let buttons=document.querySelectorAll("a");
+buttons.forEach((button)=>{
+    button.onclick = function(e){
+        let x=e.clientX - e.target.offsetLeft;
+        let y=e.clientY - e.target.offsetTop;
+        let z=e.clientZ - e.target.offsetRight;
+        let ripple=document.createElement('span');
+        ripple.style.left=`${x}px`;
+        ripple.style.top=`${y}px`;
+        ripple.style.right=`${z}px`;
+        this.appendChild(ripple);
+        setTimeout(function(){
+            ripple.remove();
+
+        },600);
+    }
+});
